7111ddae7f89fb47cf6822828027d081422da713
joeldipops <joeldipops@gmail.com>
2019-05-24 17:53:47 +1000

    mucked around with the jrAny test

--- test/test.asm
+++ test/test.asm
@@ -10,41 +10,92 @@ SECTION "Header", ROM0[$0100]
 SECTION "Home", ROM0[$0150]
 main:
     nop
+;;; 
+; H=1 N=0 and H=0 N=1 cases 
+;;;
+jrAnyTest1:
+.start
+
+    and A, A
+    jrAny NN, .nnyes
+    jp .start
+.nnyes
+    xor A, A
+    cp A,A
+    jrAny NN, .start
+
+    xor A, A
+    cp A, A
+    jrAny NH, .nhyes
+    jp .start
+.nhyes
+    and A, A
+    jrAny NH, .start
+
+    and A, A
+    jrAny H, .hyes
+    jp .start
+
+.hyes
+    xor A, A
+    cp A, A
+    jrAny H, .end
+
+    xor A, A
+    cp A, A
+    jrAny N, .nyes
+    jp .start
+.nyes
+    and A, A
+    jrAny N, .end
+
+    jp jrAnyTest2
+.end
+    jp .start
+
+;;; 
+; H=0 N=0 and H=1 N=1 cases 
+;;;
+jrAnyTest2:
+.start
     ccf
-    jrAny NN, nnyes
-    jp $0100
+    jrAny NN, .nnyes
+    jp .start
 
-nnyes:
+.nnyes
     cpl
-    jrAny NN, $0100
+    jrAny NN, .start
 
     ccf
-    jrAny NH, nhyes
-    jp $0100
-nhyes:
+    jrAny NH, .nhyes
+    jp .start
+.nhyes
     cpl
-    jrAny NH, $0100
+    jrAny NH, .start
 
     cpl
-    jrAny H, hyes
-    jp $0100
+    jrAny H, .hyes
+    jp .start
 
-hyes:
+.hyes
     ccf
-    jrAny H, end
+    jrAny H, .end
 
     cpl
-    jrAny N, nyes
-    jp $0100
-nyes:
+    jrAny N, .nyes
+    jp .end
+.nyes
     ccf
-    jrAny N, end
+    jrAny N, .end
+    jp passed
     nop
     nop
-end:
-    jp $0100
+.end
+    jp .start
 
-    ENDC
-    
+passed:
+    nop
+
+    ENDC 
     
 
